a76dc55ee2b2a480f64f5b8c1061976bd9fab643
Mario Barragán <albeto8@gmail.com>
2017-04-06 12:11:43 -0500

    Working with graphics and drawables

---
+++ app/src/main/java/com/example/mariobarragan/herome/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mariobarragan.herome.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mariobarragan.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
